58e1ffa827691a8c204978207e81a734e4a56775
Wings_Ranger <Lachlan.gaaschs@gmail.com>
2026-03-30 10:36:59 +1100

    Remove unused variables and print statements for clarity

--- index.py
+++ index.py
@@ -1,10 +1,6 @@
-x = 5
-y = "Lachlan"
 nameFirst = "Lachlan"
 nameLast = "Nash"
 nameFull = nameFirst + " " + nameLast
-print(x)
-print(y)
 print(nameFirst + " " + nameLast)
 print(nameFull)
 f =open ("test.txt", "w")
